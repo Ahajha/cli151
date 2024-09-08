f62ff5da99e58e2bb86b4cec9d298a4a56054012
Alex Trotta <ahajha@gmail.com>
2024-09-08 01:42:16 -0400

    Add cli151::meta, support designated initializers

--- include/cli151/cli151.hpp
+++ include/cli151/cli151.hpp
@@ -10,29 +10,16 @@ namespace cli151
 template <class T>
 struct arg
 {
-	T ptr_to_member;
-	// If empty, no value. Mainly applicable for short names.
-	std::string_view field_name;
-	std::string_view short_name;
+	T ptr;
 
-	// Should have a few constructors, all constexpr/consteval
-	// This constructor will only be available when reflection is available
-	// Will need to check which compilers support this
-	consteval arg(T t) : ptr_to_member{t}
-	{
-		// reflect the other fields
-	}
-
-	// This should give a compile failure if fn is length 0. Todo: verify
-	// If given a string literal, short_name ends up being a nul character
-	// Should find a way to fix that I guess.
-	consteval arg(T t, std::string_view fn)
-		: ptr_to_member{t}, field_name{fn}, short_name{fn.data(), 1}
-	{}
-	consteval arg(T t, std::string_view fn, std::string_view sn)
-		: ptr_to_member{t}, field_name{fn}, short_name{sn}
-	{}
+	// Ideally, this is the order people tend to want to use these
+	std::string_view help = "__";     // Almost always
+	std::string_view abbr = "__";     // Occasionally, to deal with conflicts
+	std::string_view arg_name = "__"; // Rarely, usually reflected
 };
+
+template <class T>
+arg(T, ...) -> arg<T>;
 
 // all args should be a cli::arg
 template <class... Ts>
@@ -57,6 +44,10 @@ struct is_member_pointer<Member Class::*> : std::true_type
 
 template <class... Ts>
 args(Ts...) -> args<std::conditional_t<detail::is_member_pointer<Ts>::value, arg<Ts>, Ts>...>;
+
+template <class T>
+struct meta
+{};
 
 // Probably want some sort of error code
 template <class T>
