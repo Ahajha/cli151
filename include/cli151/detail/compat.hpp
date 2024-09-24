143b274854e73f8730e430c336fa0cd9afc02aa5
Ahajha <ahajha@gmail.com>
2024-09-24 01:22:38 -0400

    Update compat code

--- include/cli151/detail/compat.hpp
+++ include/cli151/detail/compat.hpp
@@ -2,17 +2,27 @@
 
 // https://github.com/llvm/llvm-project/issues/62801
 // We want <expected> and C++23, unless we're on clang < 19 with libstdc++
-#if __has_include(<expected>) && __cplusplus > 202300L && \
-    !(defined __clang__&& defined __GLIBCXX__&& __clang_major__ < 19)
-#include <expected>
-namespace cli151::compat
-{
-using std::expected;
-}
+#if !__has_include(<expected>)
+#define CLI151_HAS_STD_EXPECTED false
 #else
+#include <expected>
+#if !defined __cpp_lib_expected
+#define CLI151_HAS_STD_EXPECTED false
+#elif defined __clang__ && defined __GLIBCXX__ && __clang_major__ < 19
+#define CLI151_HAS_STD_EXPECTED false
+#endif
+#endif
+
+#ifdef CLI151_HAS_STD_EXPECTED
 #include <tl/expected.hpp>
 namespace cli151::compat
 {
 using tl::expected;
+}
+#else
+#define CLI151_HAS_STD_EXPECTED true
+namespace cli151::compat
+{
+using std::expected;
 }
 #endif
