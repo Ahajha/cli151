7b708ded7c9f9d78f8af08ae5a8ef43debadc0f3
Ahajha <ahajha@gmail.com>
2024-10-03 23:37:39 -0400

    Don't use global qualifier in macro

--- include/cli151/macros.hpp
+++ include/cli151/macros.hpp
@@ -2,7 +2,7 @@
 
 #define CLI151_CLI(STRUCT_NAME, ...)                                                               \
 	template <>                                                                                    \
-	struct ::cli151::meta<STRUCT_NAME>                                                             \
+	struct cli151::meta<STRUCT_NAME>                                                               \
 	{                                                                                              \
 		using T = STRUCT_NAME;                                                                     \
 		constexpr static auto value = args{__VA_ARGS__};                                           \
