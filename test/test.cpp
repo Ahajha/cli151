5c27879380943fe46ae2ae7391faf1eb0a2708da
Ahajha <ahajha@gmail.com>
2024-10-27 18:01:46 -0400

    Simplify example file

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,6 @@
 #include <cli151/cli151.hpp>
-#include <cli151/detail/reflect.hpp>
 #include <cli151/formatters/error/format.hpp>
 
-#include <iostream>
 #include <optional>
 #include <string_view>
 
@@ -31,9 +29,7 @@ struct cli::meta<mycli>
 	};
 };
 
-auto to_string_view(frozen::string str) -> std::string_view { return {str.data(), str.size()}; }
-
-auto parse_and_print_results(const int argc, const char* const* argv) -> void
+int main(int argc, char* argv[])
 {
 	auto result = cli::parse<mycli>(argc, argv);
 
@@ -44,38 +40,10 @@ auto parse_and_print_results(const int argc, const char* const* argv) -> void
 	else
 	{
 		const auto& out = result.value();
-		std::cout << "number: " << out.number << '\n';
-		std::cout << "name: " << out.name << '\n';
-		std::cout << "author: " << out.author.value_or("No author") << '\n';
-		std::cout << "other_number: " << out.other_number << '\n';
-		std::cout << "flag: " << out.flag << '\n';
-	}
-}
-
-int main(int argc, char* argv[])
-{
-	parse_and_print_results(argc, argv);
-	// constexpr std::array args{"main", "123",      "--other-number", "456",
-	//                           "bob",  "--author", "steve",          "--flag"};
-
-	// parse_and_print_results(args.size(), args.data());
-
-	std::cout << cli::detail::get_member_name<&mycli::number>() << '\n';
-
-	static_assert(cli::detail::get_member_name<&mycli::number>() == "number");
-
-	std::cout << cli::detail::kebabbed_name<mycli, 0>::name << '\n';
-	std::cout << cli::detail::kebabbed_name<mycli, 1>::name << '\n';
-	std::cout << cli::detail::kebabbed_name<mycli, 2>::name << '\n';
-	std::cout << cli::detail::kebabbed_name<mycli, 3>::name << '\n';
-
-	static_assert(cli::detail::type_of_arg<mycli, 0>() == cli::arg_type::positional_required);
-	static_assert(cli::detail::type_of_arg<mycli, 1>() == cli::arg_type::positional_required);
-	static_assert(cli::detail::type_of_arg<mycli, 2>() == cli::arg_type::keyword);
-	static_assert(cli::detail::type_of_arg<mycli, 3>() == cli::arg_type::keyword);
-
-	for (const auto& [key, value] : cli::detail::handler_dispatcher<mycli>::long_name_to_index_map)
-	{
-		std::cout << "Key: " << to_string_view(key) << ", Value: " << value << '\n';
+		cli::compat::println("number: {}", out.number);
+		cli::compat::println("name: {}", out.name);
+		cli::compat::println("author: {}", out.author.value_or("No author"));
+		cli::compat::println("other_number: {}", out.other_number);
+		cli::compat::println("flag: {}", out.flag);
 	}
 }
