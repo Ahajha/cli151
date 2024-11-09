19515628bccd7e33984c88a5d31230504e88522e
Ahajha <ahajha@gmail.com>
2024-11-08 20:37:45 -0500

    WIP error formatting

--- include/cli151/formatters/error/format.hpp
+++ include/cli151/formatters/error/format.hpp
@@ -20,9 +20,49 @@ struct cli151::compat::formatter<cli151::error_formatter<T>, CharType>
 	{
 		assert(err.err.arg_index < err.argc);
 
+		using cli151::error_type;
 		using cli151::compat::format_to;
 
 		const auto& help_data = cli151::detail::help_data_of<T>::data;
+
+		const auto n_positional_args = std::count_if(
+			help_data.begin(), help_data.end(),
+			[](const auto& data) { return data.type == cli151::arg_type::positional_required; });
+
+		format_to(ctx.out(), "Error: ");
+		switch (err.err.type)
+		{
+		case error_type::invalid_key:
+			format_to(ctx.out(), "Unknown option \"{}\". Available options:\n",
+			          err.argv[err.err.arg_index]);
+			// TODO: print just options
+			break;
+		case error_type::not_a_number:
+			format_to(ctx.out(), "Not a number\n");
+			// Let's not overcomplicate this. Just print which one is wrong, say it should be a
+			// number. Print the usage.
+			break;
+		case error_type::number_out_of_range:
+			format_to(ctx.out(), "Number out of range\n");
+			// Similar to above
+			break;
+		case error_type::too_many_positional_args:
+			// TODO: Print the range (min and max) and the number given (once positional optional
+			// args are implemented)
+			format_to(ctx.out(), "Too many positional args, expected {}, got {}\n",
+			          n_positional_args, "too many");
+			// Print short usage
+			break;
+		case error_type::not_enough_positional_args:
+			format_to(ctx.out(), "Not enough positional args, expected {}, got {}\n",
+			          n_positional_args, "not enough");
+			// We don't actually check this yet
+			break;
+		case error_type::duplicate_arg:
+			// TODO: Aliases might make this confusing
+			format_to(ctx.out(), "Duplicate argument\n");
+			break;
+		}
 
 		format_to(ctx.out(), "{}\n", cli151::usage_formatter<T>{err.argv[0]});
 
