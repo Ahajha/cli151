856203cc4b7175948bab2ce561ac9f2731ca08ae
Ahajha <ahajha@gmail.com>
2024-11-19 03:35:03 -0500

    Fill out some TODOs with option formatting

--- include/cli151/formatters/error/format.hpp
+++ include/cli151/formatters/error/format.hpp
@@ -23,11 +23,15 @@ struct cli151::compat::formatter<cli151::error_formatter<T>, CharType>
 		using cli151::error_type;
 		using cli151::compat::format_to;
 
-		const auto& help_data = cli151::detail::help_data_of<T>::data;
+		constexpr auto help_data = cli151::detail::help_data_of<T>::data;
 
-		const auto n_positional_args = std::count_if(
+		constexpr auto n_positional_args = std::count_if(
 			help_data.begin(), help_data.end(),
 			[](const auto& data) { return data.type == cli151::arg_type::positional_required; });
+
+		constexpr auto n_keyword_args =
+			std::count_if(help_data.begin(), help_data.end(),
+		                  [](const auto& data) { return data.type == cli151::arg_type::keyword; });
 
 		format_to(ctx.out(), "Error: ");
 		switch (err.err.type)
@@ -35,8 +39,7 @@ struct cli151::compat::formatter<cli151::error_formatter<T>, CharType>
 		case error_type::invalid_key:
 			format_to(ctx.out(), "Unknown option \"{}\". Available options:\n",
 			          err.argv[err.err.arg_index]);
-			// TODO: print just options
-			break;
+			return format_options(help_data, ctx);
 		case error_type::not_a_number:
 			format_to(ctx.out(), "Not a number\n");
 			// Let's not overcomplicate this. Just print which one is wrong, say it should be a
@@ -66,25 +69,39 @@ struct cli151::compat::formatter<cli151::error_formatter<T>, CharType>
 
 		format_to(ctx.out(), "{}\n", cli151::usage_formatter<T>{err.argv[0]});
 
-		// TODO: skip this one if no positional args
-		format_to(ctx.out(), "Positional arguments:\n");
-		for (const auto& data : help_data)
+		if constexpr (n_positional_args != 0)
 		{
-			if (data.type == cli151::arg_type::positional_required)
+			format_to(ctx.out(), "Positional arguments:\n");
+			for (const auto& data : help_data)
 			{
-				// TODO: This is kebabbed, don't want that
-				format_to(ctx.out(), "    {}", data.name);
-
-				if (data.help != cli151::default_)
+				if (data.type == cli151::arg_type::positional_required)
 				{
-					format_to(ctx.out(), ": {}", data.help);
+					// TODO: This is kebabbed, don't want that
+					format_to(ctx.out(), "    {}", data.name);
+
+					if (data.help != cli151::default_)
+					{
+						format_to(ctx.out(), ": {}", data.help);
+					}
+					format_to(ctx.out(), "\n");
 				}
-				format_to(ctx.out(), "\n");
 			}
 		}
 
-		// TODO: Skip if no keyword args
-		format_to(ctx.out(), "Options:\n");
+		if constexpr (n_keyword_args != 0)
+		{
+			format_to(ctx.out(), "Options:\n");
+			format_options(help_data, ctx);
+		}
+
+		return ctx.out();
+	}
+
+  private:
+	template <class context>
+	constexpr auto format_options(const auto& help_data, context& ctx) const ->
+		typename context::iterator
+	{
 		for (const auto& data : help_data)
 		{
 			if (data.type == cli151::arg_type::keyword)
